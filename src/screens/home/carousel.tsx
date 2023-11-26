c837300ac8c38346b1b39987a78cffece40af1bf
Muhammed Çiçekel <muhammedcicekel@gmail.com>
2023-11-26 21:42:54 +0300

    A page indicator was added to the carousel on the homepage.

--- src/screens/home/carousel.tsx
+++ src/screens/home/carousel.tsx
@@ -1,7 +1,7 @@
 import { Dimensions } from 'react-native'
-import React from 'react'
-import Carousel from 'react-native-reanimated-carousel';
-import { Image } from '@/ui'
+import React, {useState, useRef} from 'react'
+import Carousel, { ICarouselInstance } from 'react-native-reanimated-carousel';
+import { Image, PageIndicator, View } from '@/ui'
 
 export const NCarousel = () => {
   const width = Dimensions.get('window').width;
@@ -11,8 +11,12 @@ export const NCarousel = () => {
     require('../../../assets/home-carousel-2.jpeg'),
     require('../../../assets/home-carousel-3.jpeg'),
   ]
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const carouselRef = useRef<ICarouselInstance>(null);
+
   return (
-    <Carousel
+    <View>
+      <Carousel
             loop
             width={width}
             height={width / 2}
@@ -22,6 +26,13 @@ export const NCarousel = () => {
             renderItem={({ item }) => (
               <Image source={item} className="w-auto h-full mx-4 rounded-md"/>
             )}
+            ref={carouselRef}
+          onProgressChange={() => {
+            const index = carouselRef.current?.getCurrentIndex() ?? 0;
+            setCurrentIndex(index);
+          }}
           />
+           <PageIndicator currentIndex={currentIndex} totalItems={carouselData.length}/>
+    </View>
   )
 }
