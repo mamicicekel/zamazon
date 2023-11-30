3708ea051fbb044f4267a5e2816a13ff240fca11
Muhammed Çiçekel <muhammedcicekel@gmail.com>
2023-11-30 12:19:47 +0300

    Cart screen is finished.

--- src/screens/cart/index.tsx
+++ src/screens/cart/index.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
-import { Image, SafeAreaView, Text, View } from '@/ui';
+import { Button, Image, SafeAreaView, ScrollView, Text, View } from '@/ui';
 import { Box } from './box';
 import { useCartStore } from '@/core/cartStore';
 
 export const Cart = () => {
   const cartItems = useCartStore((state) => state.cartItems);
+  const totalPrice = cartItems.reduce((total, item) => total + item.quantity*item.price, 0)
+
 
   return (
     <SafeAreaView className={`flex-1 ${cartItems.length> 0 ? 'justify-start' : 'justify-center'}`}>
       {cartItems.length > 0 ? (
-        <View>
+        <>
           <Text className='text-3xl m-6 font-semibold'>Cart</Text>
+          <ScrollView>
           {cartItems.map((item, index) => (
-            <Box key={index} {...item} />
+            <Box key={index} {...item}/>
           ))}
-          
-        </View>
+          </ScrollView>
+          <View className='flex-row justify-between mx-4 mb-4 items-center'>
+            <View>
+              <Text className='text-primary-900'>Selected products({cartItems.length})</Text>
+              <Text className='font-semibold text-3xl'>${totalPrice}</Text>
+            </View>
+            <Button label='Done' variant='secondary'/>
+          </View>
+        </>
       ) : (
         <>
           <Image source={{ uri: 'https://cdni.iconscout.com/illustration/premium/thumb/empty-cart-2130356-1800917.png' }} className='w-full h-64' />
